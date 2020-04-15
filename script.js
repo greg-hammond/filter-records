ec1757ca6327acae13a17186d82b389f950613d8
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2020-04-15 05:48:41 -0500

    misc cleanup

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 
 
-const init = async () => {
+const init = () => {
 
     const data = __dataBank[161];
-    let {title, columns, records} = data; // object destructuring assignment
+    let { title, columns, records } = data; // object destructuring assignment
 
     const filterDiv = document.querySelector("#filters");
-    
+
     // this will house an array of records in the DOM (after built)
     let recordList = {};
 
     // grab all column names that are defined as filters.
-    const filterKeys = columns.filter(col => col.filter ).map(col => col.name);
+    const filterKeys = columns.filter(col => col.filter).map(col => col.name);
 
 
     // build list of unique values for each filter key/col
     // in form values[key] = [val1, val2, val3, ...]
     // this is built only for purposes of building the filter section UI.
+    // @@@todo: sort values within each filter
     let values = [];
-    let ids = 0;
     records.forEach(rec => {
-        rec.id = ids++; // assign id to record, and data-id to record divs below
-        filterKeys.forEach( key => {
+        filterKeys.forEach(key => {
             const value = rec[key];
             values[key] = values[key] || [];
             if (values[key].indexOf(value) == -1) {
-                values[key] = [...values[key], value];                    
+                values[key] = [...values[key], value];
             }
         })
     });
@@ -36,7 +35,7 @@ const init = async () => {
 
 
     //----- build the filter UI
-    filterKeys.forEach( key => {
+    filterKeys.forEach(key => {
 
         let div = document.createElement("div");
         div.classList.add("keyval-list");
@@ -77,14 +76,11 @@ const init = async () => {
 
         const listDiv = document.querySelector("#records");
 
-        records.forEach( rec => {
-            let div = document.createElement("div");
-            div.classList.add("record", "row", "selected");
-            div.setAttribute("data-id", rec.id);  // so I can connect records and divs
-            columns.forEach( col => {
+        records.forEach(rec => {
+            let div = addElem(listDiv, "div", "", "record", "row", "selected");
+            columns.forEach(col => {
                 addElem(div, "p", rec[col.name], "cell", col.name)
             });
-            listDiv.append(div);
         });
 
         // used for filtering and sorting later
@@ -101,7 +97,7 @@ const init = async () => {
         // loop through all buttons for this filter value group
         // set the clicked one as selected, all others as not.
 
-        if(this.classList.contains("selected")) {
+        if (this.classList.contains("selected")) {
             // we clicked to de-select the already-selected one.
             this.classList.remove("selected");
 
@@ -109,7 +105,7 @@ const init = async () => {
             // we select the one that was clicked, de-select all others.
             this.parentNode.querySelectorAll(".val").forEach(itm => {
                 itm.classList.toggle("selected", itm === this);
-            });    
+            });
         }
 
         // now filter recs against this value
@@ -117,45 +113,7 @@ const init = async () => {
     }
 
 
-    // column header click handler
-    // sort by clicked column - attach "ascending" class to the .header.cell
-    // again, old-school function syntax to preserve 'this'
-    // easiest will be to sort input array and rebuild/replace the DOM, i think...
-    // nope.  rebuilding kills our filtering.  need to sort in place.
-    function sortColumn() {
 
-        // figure out which column we're sorting...        
-        const sortKey = this.textContent;
-        // get the class name (sort key with spaces xlated to dashes)
-        // see buildRecords above
-        //const clsName = sortKey.split(" ").join("-");
-        //console.log(clsName);
-
-        const column = columns.find( col => col.name === sortKey);
-        // ... and whether it's numeric...
-        const isNumeric = (column) ? column.numeric : false;
-        // ... and which way (asc or desc)
-        this.classList.toggle("ascending");
-        const sortDir = this.classList.contains("ascending") ? -1 : 1;
-
-        recordList.sort( (a, b) => {
-                
-            // I stuffed the col name as a class for each cell.
-            // so now we can retrieve the column we want using getElementsByClassName.
-            // this returns an HTML collection, so we take the [0]th one.
-            let aval = a.getElementsByClassName(sortKey)[0].textContent;
-            let bval = b.getElementsByClassName(sortKey)[0].textContent;
-
-            return (isNumeric) ? 
-                (+aval < +bval ? sortDir : -sortDir) :
-                ( aval.toUpperCase() < bval.toUpperCase()) ? sortDir : -sortDir;
-        })
-
-        // finally, map to reinsert back into container, in order.
-        .map( itm => itm.parentNode.appendChild(itm));
-
-
-    }
 
 
     //----- filter record display based on selected filters
@@ -176,7 +134,7 @@ const init = async () => {
 
         // loop on records and perform filtering
 
-        recordList.forEach( rec => {
+        recordList.forEach(rec => {
             let pass = true;
             Object.keys(filters).forEach(key => {
                 let val = filters[key];
@@ -194,11 +152,44 @@ const init = async () => {
         });
     }
 
+
+    // column header click handler
+    //
+    function sortColumn() {
+
+        // figure out which column we're sorting...        
+        const sortKey = this.textContent;
+        const column = columns.find(col => col.name === sortKey);
+        // ... and whether it's numeric...
+        const isNumeric = (column) ? column.numeric : false;
+        // ... and which way (asc or desc)
+        this.classList.toggle("ascending");
+        const sortDir = this.classList.contains("ascending") ? -1 : 1;
+
+        recordList.sort((a, b) => {
+
+            // I stuffed the col name as a class for each cell.
+            // so now we can retrieve the column we want using getElementsByClassName.
+            // this returns an HTML collection, so we take the [0]th one.
+            let aval = a.getElementsByClassName(sortKey)[0].textContent;
+            let bval = b.getElementsByClassName(sortKey)[0].textContent;
+
+            return (isNumeric) ?
+                (+aval < +bval ? sortDir : -sortDir) :
+                (aval.toUpperCase() < bval.toUpperCase()) ? sortDir : -sortDir;
+        })
+
+            // finally, map to reinsert back into container, in order.
+            .map(itm => itm.parentNode.appendChild(itm));
+
+
+    }
+
     // build record DOM
     buildHeader();
     buildRecords();
 
-}
+} // end init
 
 
 // helper function.  create element of 'type' with 'text' content, and append to 'parent'
